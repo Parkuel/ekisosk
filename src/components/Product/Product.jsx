14e361544a8fccf43c026797bc4ebb6988d5b619
Parkuel <taofdaydisu@gmail.com>
2022-10-19 07:19:12 +0300

    feat: style home and product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -4,7 +4,16 @@ import "./Product.css";
 function Product() {
     return (
         <div className="product">
-
+            <div className="product_info">
+                <p className="product_title">Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)</p>
+                <p className="product_price">
+                    <small>$</small>
+                    <strong>30</strong>
+                </p>
+                <p className="product_rating">⭐⭐⭐⭐</p>
+            </div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="Product" className="product_image" />
+            <button className="product_button">Add to Basket</button>
         </div>
     )
 }
