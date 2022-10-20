af5a6ad1ddf003febcd844a3725f89b4e14af4ae
Parkuel <taofdaydisu@gmail.com>
2022-10-20 06:54:09 +0300

    feat: implemented props for dynamic product info

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product(props) {
     return (
         <div className="product">
             <div className="product_info">
-                <p className="product_title">Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)</p>
+                <p className="product_title">{props.title}</p>
                 <p className="product_price">
                     <small>$</small>
-                    <strong>30</strong>
+                    <strong>{props.price}</strong>
                 </p>
-                <p className="product_rating">⭐⭐⭐⭐</p>
+                <p className="product_rating">
+                    {Array(props.rating)
+                    .fill()
+                    .map(() => (
+                        "⭐"
+                    // <p>⭐</p>
+                    ))}
+                </p>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="Product" className="product_image" />
+            <img src={props.image} alt="" />
             <button className="product_button">Add to Basket</button>
         </div>
     )
