2dfd1f01700b13085053f0215286df7f2f1292ee
Parkuel <taofdaydisu@gmail.com>
2022-11-02 13:11:42 +0200

    feat: create checkout product

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct/CheckoutProduct";
 
 function Checkout() {
     return (
-        <h1>Checkout</h1>
+        <div className="checkout">
+            <div className="checkout_left">
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="advert" className="checkout_ad" />
+                <h2>Your Shopping Basket</h2>
+                <CheckoutProduct />
+
+            </div>
+
+            <div className="checkout_right">
+
+            </div>
+        </div>
     )
 }
 
